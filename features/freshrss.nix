7562369ed72dfc22d8fe67d84407eb26769905e0
harry <harryh@ik.me>
2024-07-15 18:47:34 +0200

    added required passwordFile property

--- features/freshrss.nix
+++ features/freshrss.nix
@@ -1,8 +1,16 @@
 { config, lib, ... }:
 {
+  sops.secrets = {
+    path = ../secrets/freshrss.json;
+    freshrss_username = { };
+    freshrss_password = { };
+  };
+
   services.freshrss = {
     enable = true;
     language = "fr";
+    defaultUser = config.sops.freshrss_username;
+    passwordFile = config.sops.freshrss_password.path;
     database = {
       type = "sqlite";
     };
