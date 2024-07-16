669ffa7c13d624027c19136c833229da04b6e8b3
harry <harryh@ik.me>
2024-07-16 09:08:54 +0200

    fixed path file type (was requiring str, got path)

--- features/freshrss.nix
+++ features/freshrss.nix
@@ -15,7 +15,7 @@
     enable = true;
     language = "fr";
     defaultUser = config.sops.secrets.freshrss_username;
-    passwordFile = config.sops.secrets.freshrss_password.path;
+    passwordFile = builtins.toString config.sops.secrets.freshrss_password.path;
     database = {
       type = "sqlite";
     };
