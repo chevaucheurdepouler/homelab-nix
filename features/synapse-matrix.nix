f7621c2bad5e3aad341cd96b7f12247dad73ab0e
harry <harryh@ik.me>
2024-07-18 17:17:01 +0200

    basic matrix config file

--- features/synapse-matrix.nix
+++ features/synapse-matrix.nix
@@ -1,4 +1,14 @@
 { config, lib, ... }:
 #TODO: implement 
 {
+  services.matrix-synapse = {
+    enable = true;
+    settings = {
+      serverName = "talk.hypervirtual.world";
+    };
+
+    redis = {
+      enabled = true;
+    };
+  };
 }
