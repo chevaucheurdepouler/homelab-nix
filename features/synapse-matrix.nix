633dc9a34d7c3a83495b737a26ef55c5f6b489a9
harry <harryh@ik.me>
2024-07-18 22:40:20 +0200

    added missing required options

--- features/synapse-matrix.nix
+++ features/synapse-matrix.nix
@@ -3,6 +3,8 @@
 {
   services.matrix-synapse = {
     enable = true;
+    enable_registration = false;
+
     settings = {
       serverName = "talk.hypervirtual.world";
     };
@@ -10,5 +12,11 @@
     redis = {
       enabled = true;
     };
+    extras = [
+      "systemd"
+      "postgres"
+      "url-preview"
+      "user-search"
+    ];
   };
 }
