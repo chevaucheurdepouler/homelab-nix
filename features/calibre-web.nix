e2871606a5c81b81b3af7bf85765ddf55e7df375
harry <harryh@ik.me>
2024-07-22 11:29:41 +0200

    fixed listening ip

--- features/calibre-web.nix
+++ features/calibre-web.nix
@@ -4,6 +4,7 @@
   services.calibre-web = {
     enable = true;
     openFirewall = true;
+    listen.ip = "0.0.0.0";
     options = {
       enableBookUploading = true;
       enableKepubify = true;
