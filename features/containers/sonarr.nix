74c40487fbda51f5dc8ecdfb4bf96b7a9ba8573f
harry <harryh@ik.me>
2024-08-01 14:52:03 +0200

    fixed port conflicty

--- features/containers/sonarr.nix
+++ features/containers/sonarr.nix
@@ -6,7 +6,7 @@
       sonarrAnime = {
         image = "lscr.io/linuxserver/sonarr:latest";
         volumes = [ "sonarr_data:/config" "/srv/Multimedia/SeriesTV:/tv"  "/srv/Multimedia/Torrents:/downloads" ];
-        ports = [ "8888:80" ];
+        ports = [ "8999:80" ];
         environment = {
           "PUID" = "1000";
           "GUID" = "1000";
