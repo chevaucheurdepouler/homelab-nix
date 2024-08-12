ba3dfa1528fa94959fdbf3fa8207a8c61a21e162
harry <harryh@ik.me>
2024-08-12 16:06:40 +0200

    fixed missing option

--- features/services/forgejo.nix
+++ features/services/forgejo.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 {
-  sops.secrets.smtp_address = {};
-  sops.secrets.smtp_password = {};
+  sops.secrets.smtp_address = { };
+  sops.secrets.smtp_password = { };
 
   services.forgejo = {
     enable = true;
@@ -10,22 +10,21 @@
     database = {
       type = "postgres";
     };
-    server = {
-      DOMAIN = "git.hypervirtual.world";
-      ROOT_URL = "https://hypervirtual.world";
-      HTTP_PORT = 3000;
-    };
-
-    actions = {
-      ENABLED = true;
-      DEFAULT_ACTIONS_URL = "github";
-    };
-
-    mailer = {
+    settings = {
+      server = {
+        DOMAIN = "git.hypervirtual.world";
+        ROOT_URL = "https://hypervirtual.world";
+        HTTP_PORT = 3000;
+      };
+      actions = {
+        ENABLED = true;
+        DEFAULT_ACTIONS_URL = "github";
+      };
+      mailer = {
         ENABLED = true;
         SMTP_ADDR = config.sops.secrets.smtp_address;
       };
-
+    };
     mailerPasswordFile = config.sops.secrets.smtp_password.path;
   };
 }
