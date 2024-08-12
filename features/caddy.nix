12b1ae8207cb73307734a51a48b7449ac90a4a6e
harry <harryh@ik.me>
2024-08-12 21:12:38 +0200

    feat: exposing urls instead of ports

--- features/caddy.nix
+++ features/caddy.nix
@@ -2,12 +2,29 @@
 {
   services.caddy = {
     enable = true;
-    virtualHosts.":80".extraConfig = ''
-      reverse_proxy :8082
-    '';
 
     virtualHosts.":5050".extraConfig = ''
       reverse_proxy :8083
+    '';
+
+    virtualHosts."sisyphe.normandy.hypervirtual.world".extraConfig = ''
+      reverse_proxy :8003
+    '';
+
+    virtualHosts."git.hypervirtual.world".extraConfig = ''
+      reverse_proxy :3333
+    '';
+
+    virtualHosts."photos.hypervirtual.world".extraConfig = ''
+      reverse_proxy :2342
+    '';
+
+    virtualHosts."books.hypervirtual.world".extraConfig = ''
+      reverse_proxy :8083
+    '';
+
+    virtualHosts."fish.hypervirtual.world".extraConfig = ''
+      reverse_proxy :3030
     '';
   };
 
