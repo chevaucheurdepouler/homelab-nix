e4c4d7b3410cbae490582a397b04602cc7f7c8ec
harry <harryh@ik.me>
2024-08-12 21:15:24 +0200

    feat: added heic format support

--- features/services/nextcloud.nix
+++ features/services/nextcloud.nix
@@ -1,14 +1,37 @@
 { config, ... }:
 {
+  imports = [
+    "${
+      fetchTarball {
+        url = "https://github.com/onny/nixos-nextcloud-testumgebung/archive/fa6f062830b4bc3cedb9694c1dbf01d5fdf775ac.tar.gz";
+        sha256 = "0gzd0276b8da3ykapgqks2zhsqdv4jjvbv97dsxg0hgrhb74z0fs";
+      }
+    }/nextcloud-extras.nix"
+  ]; # adding caddy support
+
   sops.secrets.adminNextcloudPass = { };
   services.nextcloud = {
-
     enable = true;
     hostName = "cloud.hypervirtual.world";
     database.createLocally = true;
+    webserver = "caddy";
+    configureRedis = true;
     config = {
       dbtype = "pgsql";
       adminpassFile = config.sops.secrets.adminNextcloudPass.path;
     };
+    extraOptions.enabledPreviewProviders = [
+      "OC\\Preview\\BMP"
+      "OC\\Preview\\GIF"
+      "OC\\Preview\\JPEG"
+      "OC\\Preview\\Krita"
+      "OC\\Preview\\MarkDown"
+      "OC\\Preview\\MP3"
+      "OC\\Preview\\OpenDocument"
+      "OC\\Preview\\PNG"
+      "OC\\Preview\\TXT"
+      "OC\\Preview\\XBitmap"
+      "OC\\Preview\\HEIC"
+    ];
   };
 }
