3df10b933d7757533bbbcff4350c2301f59bc087
harry <harryh@ik.me>
2024-09-08 11:41:01 +0200

    updated goober config

--- flake.nix
+++ flake.nix
@@ -9,8 +9,13 @@
     alejandra.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-secrets = {
-      url = "git+https://git.hypervirtual.world/harry123/nix-secrets.git";
+      url = "git+https://git.hypervirtual.world/harry123/nix-secrets.git"; # replace with your own repo
       flake = false;
+    };
+
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
@@ -41,6 +46,8 @@
           ];
         };
 
+        # athena = nixpkgs.lib.nixosSystem {}:
+
         labouse = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
@@ -50,11 +57,36 @@
 
         goober = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
+          specialArgs = specialArgs;
+          modules = [
+            ./hosts/goober/configuration.nix
+          ];
+        };
+
+      };
+
+      isos = {
+        goober = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
           modules = [
             ./features/isos/goober.nix
           ];
         };
       };
 
+      proxmox = {
+        sisyphe = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          specialArgs = {
+            diskSize = 600 * 1024;
+            inherit specialArgs;
+          };
+          modules = [
+            ./hosts/sisyphe/configuration.nix
+            sops-nix.nixosModules.sops
+          ];
+          format = "proxmox";
+        };
+      };
     };
 }
