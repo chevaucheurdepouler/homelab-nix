a2f761d5ef0e367ac407e9b719e2345ae632b45c
harry <harryh@ik.me>
2024-10-12 22:11:54 +0200

    small update to proxmox image

--- hosts/dionysos/configuration.nix
+++ hosts/dionysos/configuration.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  username,
+  ...
+}:
 {
   environment.systemPackages = with pkgs; [
     weechat
@@ -10,4 +15,17 @@
     enable = true;
     clock24 = true;
   };
+
+  system.stateVersion = "24.05";
+
+  users.users.${username} = {
+    isNormalUser = true;
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8sdToNavEQv7PTMJ97HIGM6UlChwGS3x9O8hFilzui harryh@ik.me"
+    ];
+  };
+
+  users.users.${username}.initialHashedPassword = "$y$j9T$s4isXqWcg4N8TEPjmj0fD/$zog2cpUwstnvwDnQsFmH3br/WAeD2Uu/L7ePr00cKkA";
+  environment.variables.EDITOR = "nvim";
 }
