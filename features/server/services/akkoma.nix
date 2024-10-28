1761c29ad5797f0684d666658be46ea2c44e438a
kity <geekcat@protonmail.com>
2024-10-28 10:59:08 +0100

    added new feature, planning for firefish migration

---
+++ features/server/services/akkoma.nix
@@ -0,0 +1,17 @@
+{
+  services.akkoma.enable = true;
+  services.akkoma.config = {
+    ":pleroma" = {
+      ":instance" = {
+        name = "e^akkoma + 1 = 0";
+        description = "the cuntiest french akkoma instance<3. centres d'intérêts : la mode, les sciences, la musique. mais ça ne vous empêche pas de parler de n'importe quoi! join us !";
+        email = "admin@babychou.me";
+        registration_open = false;
+      };
+
+      "Pleroma.Web.Endpoint" = {
+        url.host = "fish.hypervirtual.word";
+      };
+    };
+  };
+}
