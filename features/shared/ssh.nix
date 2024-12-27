c84a24c2ac6bf8159b213aab4896af36e2839564
kity <geekcat@protonmail.com>
2024-12-27 14:34:19 +0100

    added ssh jail

--- features/shared/ssh.nix
+++ features/shared/ssh.nix
@@ -8,4 +8,14 @@
       PermitRootLogin = "no";
     };
   };
+
+  services.fail2ban.jails.sshd.settings = {
+    ssh = ''
+    enabled = true
+    port = ssh
+    filter = sshd
+    logpath = %(sshd_log)s
+    maxretry = 5
+    '';
+  };
 }
