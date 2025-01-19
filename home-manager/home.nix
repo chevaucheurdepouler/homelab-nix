7cd9906bc1ec580f8a17d35438f109a97e74790a
harry123 <harryh@ik.me>
2025-01-19 20:14:03 +0100

    added krita

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -74,6 +74,8 @@
 
     pkgs.audacious
     pkgs.audacious-plugins
+
+    pkgs.krita
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
