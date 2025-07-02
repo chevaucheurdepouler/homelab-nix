3d6286340fc3436ffbe3d33233887977359af082
harry123 <harryh@ik.me>
2025-07-02 18:28:00 +0200

    fixed niri path

--- hosts/workstation/configuration.nix
+++ hosts/workstation/configuration.nix
@@ -2,12 +2,12 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{inputs, config, pkgs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
-      ../../shared/client/tailscale.nix
+      ../../shared/client/niri.nix
       ./hardware-configuration.nix
     ];
 
