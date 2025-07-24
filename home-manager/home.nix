b615595b14d614eb2e7d9189f5d375e0980d76cc
harry123 <harryh@ik.me>
2025-07-24 18:41:27 +0200

    fixed conflicts

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -87,6 +87,7 @@
       obsidian
       papirus-icon-theme
       nvimpager
+      mpc
       # # You can also create simple shell scripts directly inside your
       #  # configuration. For example, this adds a command 'my-hello' to your
       # # environment:
