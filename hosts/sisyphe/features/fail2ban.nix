16d00b902b6dc797d5ea1bd9ce407323c738f31a
kity <geekcat@protonmail.com>
2025-08-07 17:36:34 +0200

    disabled nextcloud for the moment

--- hosts/sisyphe/features/fail2ban.nix
+++ hosts/sisyphe/features/fail2ban.nix
@@ -5,24 +5,28 @@
     ignoreIP = [ "192.168.1.0/24" ];
     extraPackages = [ ];
     jails = {
-      nextcloud = ''
-        enabled = true;
-        filter = nextcloud
-        port = http,https
-      '';
-     };
+      /*
+        nextcloud = ''
+             enabled = true;
+             filter = nextcloud
+             port = http,https
+           '';
+      */
+    };
   };
 
   environment.etc = {
+    /*
       "fail2ban/filter.d/nextcloud.conf".text = ''
-        [Definition]
-        _groupsre = (?:(?:,?\s*"\w+":(?:"[^"]+"|\w+))*)
-        datepattern = ,?\s*"time"\s*:\s*"%%Y-%%m-%%d[T ]%%H:%%M:%%S(%%z)?"
-        failregex = ^[^{]*\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message":"Login failed:
-                    ^[^{]*\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message":"Trusted domain error.
-                    ^[^{]*\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message":"Two-factor challenge failed:
-        journalmatch = _SYSTEMD_UNIT=phpfpm-nextcloud.service
-      '';
-    };
+           [Definition]
+           _groupsre = (?:(?:,?\s*"\w+":(?:"[^"]+"|\w+))*)
+           datepattern = ,?\s*"time"\s*:\s*"%%Y-%%m-%%d[T ]%%H:%%M:%%S(%%z)?"
+           failregex = ^[^{]*\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message":"Login failed:
+                       ^[^{]*\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message":"Trusted domain error.
+                       ^[^{]*\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message":"Two-factor challenge failed:
+           journalmatch = _SYSTEMD_UNIT=phpfpm-nextcloud.service
+         '';
+    */
+  };
 
 }
