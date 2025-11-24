d2297173a34699bfe960c7bc22e2c75b81149698
misschloe777 <harryh@ik.me>
2025-11-24 15:51:21 +0100

    added fuzzel-emoji

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -122,7 +122,7 @@
     ".config/hyfetch.json".source = dotfiles/hyfetch/hyfetch.json;
     ".config/niri".source = dotfiles/niri;
     ".config/mako".source = dotfiles/mako;
-    ".scripts/" = ./dotfiles/scripts;
+    ".scripts/".source = dotfiles/scripts;
   };
 
   # programs.matugen = {
