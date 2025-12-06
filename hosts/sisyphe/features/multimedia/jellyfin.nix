380eb518df9da24225bfa76dcb33b51590c63ae3
clo <clo.h@gozmail.bzh>
2025-12-06 01:39:33 +0100

    fix: driver changed name

--- hosts/sisyphe/features/multimedia/jellyfin.nix
+++ hosts/sisyphe/features/multimedia/jellyfin.nix
@@ -17,7 +17,7 @@
     extraPackages = with pkgs; [
       intel-media-driver
       intel-vaapi-driver # previously vaapiIntel
-      vaapiVdpau
+      libva-vdpau-driver
       intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
       intel-media-sdk # QSV up to 11th gen
     ];
