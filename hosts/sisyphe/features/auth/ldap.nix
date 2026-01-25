b5427c6c33f3e2bdad2073d36a05c78179b5bde1
clo <clo.h@gozmail.bzh>
2026-01-25 11:23:22 +0100

    fix: perms error while setting secret

--- hosts/sisyphe/features/auth/ldap.nix
+++ hosts/sisyphe/features/auth/ldap.nix
@@ -2,9 +2,11 @@
 {
   sops.secrets."ldap_password" = {
     sopsFile = "${secrets-next}/secrets/bordel.yaml";
+    owner = "lldap";
   };
   sops.secrets."ldap_seed_file" = {
     sopsFile = "${secrets-next}/secrets/bordel.yaml";
+    owner = "lldap";
   };
   services.lldap = {
     enable = true;
