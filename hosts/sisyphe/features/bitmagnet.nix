d47bcf24b213993b8d0347546dba78d9121149dc
clo <clo.h@gozmail.bzh>
2026-01-25 12:08:02 +0100

    fix: bitmagnet is not reachable

--- hosts/sisyphe/features/bitmagnet.nix
+++ hosts/sisyphe/features/bitmagnet.nix
@@ -7,6 +7,8 @@
     useLocalPostgresDB = true;
     settings = {
       # extra_config_files = "${secrets}/secrets/bitmagnet.env";
+      http_server.local_address = "0.0.0.0";
+      http_server.port = ":3333";
     };
   };
 }
