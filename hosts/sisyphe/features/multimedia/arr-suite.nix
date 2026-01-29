21b005bfa64afc00acfee5abe03a3f9c40544d17
clo <clo.h@gozmail.bzh>
2026-01-29 18:52:08 +0100

    nix told me to do that

--- hosts/sisyphe/features/multimedia/arr-suite.nix
+++ hosts/sisyphe/features/multimedia/arr-suite.nix
@@ -13,6 +13,7 @@ let
       version = "3.1.2.4902";
       src = builtins.fetchurl {
         url = "http://lidarr.servarr.com/v1/update/develop/updatefile?os=linux&runtime=netcore&arch=x64";
+        name = "lidarr-nightly";
         sha256 = "sha256-1oSFJtQ9/ICACFBFgp2kPzfP0dKitMg7vqAN7iT0KTA=";
       };
     }
